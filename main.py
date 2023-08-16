55533ee7b017343ee33c0b468b71a738d1dc0b1b
Portoouu <127618106+Portoouu@users.noreply.github.com>
2023-08-15 22:11:00 -0300

    main.py
    Adicionado opcao_2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def opcao_1():
     print("Você escolheu a opção 1.")
 
+def opcao_2():
+    print("Você escolheu a opção 2.")
+
 def exibir_menu():
     print("Escolha uma opção:")
     print("1. Opção 1")
+    print("2. Opção 2")
     print("0. Sair")
     
     escolha = int(input("Digite o número da opção desejada: "))
@@ -19,6 +23,8 @@ def main():
         elif escolha == 0:
             print("Saindo do programa.")
             break
+        elif escolha == 2:
+            opcao_2()
         else:
             print("Opção inválida. Escolha um número válido.")
 
